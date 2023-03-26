ca332e1a77a481c3849f136233ce529b23390f6c
arktony <arktumwesigye@gmail.com>
2023-03-26 16:58:21 +0300

    Create custom hook to fetch games

---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,14 @@
+import useGames from "../hooks/useGames";
+
+const GameGrid = () => {
+  const { games, error } = useGames();
+  return (
+    <ul>
+      {games.map((game) => (
+        <li key={game.id}>{game.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GameGrid;
---
+++ src/services/api-client.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+  params: {
+    // baseURL: "https://api.rawg.io/api",
+    key: "df63a97d365d4298a4a303a10adce811",
+  },
+});
